586c6ebdacbe2d45d07476f549a876a8f169165e
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-07-02 23:43:14 +0900

    usernameとpasswordを環境変数から引用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  protect_from_forgery with: :exception
 
   private
 
   def basic_auth
-    binding.pry()
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '0622'
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 end
