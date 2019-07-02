afe615864caf635eee1c429c8308613044b951c7
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-07-02 22:25:23 +0900

    basic認証、binding.pryのgem導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    binding.pry()
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '0622'
+    end
+  end
 end
