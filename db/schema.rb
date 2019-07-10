a9e5b0384a4b74bf2e7c8b633056cf5ea689ca58
yurifukuchi <uro_409@outlook.jp>
2019-07-11 02:21:48 +0900

    create product_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_07_10_163536) do
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["name"], name: "index_products_on_name"
+  end
 
 end
