0ce93c3a6dca6f8ce27265c798168c4c77956fd2
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-07-11 23:18:49 +0900

    deviseのストロングパラメータ設定関数を移動

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :phone, :birthday])
-  end
-
   private
 
   def production?
