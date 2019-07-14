8a98c2d95cffdf234bd94ecb436e0109be91053d
Daisugi <volble124@gmail.com>
2019-07-14 20:02:21 +0900

    add-get-items-to-controller
    ピックアップアイテム一覧を取得する処理を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,21 @@
 class ItemsController < ApplicationController
 
   def index
+    # レディース
+    @ladies_products = getItems(1)
+    # メンズ
+    @mens_products = getItems(2)
+    # ベビー・キッズ
+    @kids_products = getItems(3)
+    # コスメ・香水・美容
+    @cosmes_products = getItems(4)
+  end
+  
+  private
+
+  # ピックアップアイテムに表示する一覧を取得する
+  def getItems(categoryId)
+    return TblProduct.joins(:mst_major_category, :tbl_product_images).merge(MstMajorCategory.where(id: categoryId)).distinct.order(id: "desc").limit(4)
   end
 
 end
