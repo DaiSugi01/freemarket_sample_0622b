86f1535dcd84a8ee0787c46a2d14c25c0ace8ad9
Daisugi <volble124@gmail.com>
2019-07-14 20:03:48 +0900

    add-get-brands-to-controller
    ピックアップブランド一覧用のデータ取得処理を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,14 @@ class ItemsController < ApplicationController
     @kids_products = getItems(3)
     # コスメ・香水・美容
     @cosmes_products = getItems(4)
+    # シャネル
+    @chanel_products = getBlands(1)
+    # ルイ ヴィトン
+    @vuitton_products = getBlands(2)
+    # シュプリーム
+    @supreme_products = getBlands(3)
+    # ナイキ
+    @nike_products = getBlands(4)
   end
   
   private
@@ -16,6 +24,11 @@ class ItemsController < ApplicationController
   # ピックアップアイテムに表示する一覧を取得する
   def getItems(categoryId)
     return TblProduct.joins(:mst_major_category, :tbl_product_images).merge(MstMajorCategory.where(id: categoryId)).distinct.order(id: "desc").limit(4)
+  end
+
+  # ピックアップブランドに表示する一覧を取得する
+  def getBlands(blandId)
+    return TblProduct.joins(:mst_brand, :tbl_product_images).merge(MstBrand.where(id: blandId)).distinct.order(id: "desc").limit(4)
   end
 
 end
