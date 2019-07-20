4acea8754e6cc4c4f15ab92f1cfebfb30d215d75
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-07-20 13:00:48 +0900

    会員情報新規登録後リダイレクト先を住所登録画面に変更

--- app/controllers/tbl_user/registrations_controller.rb
+++ app/controllers/tbl_user/registrations_controller.rb
@@ -5,29 +5,22 @@ class TblUser::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
   before_action :configure_permitted_parameters
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :phone, :birthday])
-  end
-
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
-
-  # POST /resource
   def create
     super
     profile = TblProfile.new(nickname: params[:nickname], tbl_user_id: resource.id)
     profile.save
   end
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  private
+  def after_sign_up_path_for(resource)
+    address_path
+  end
 
-  # PUT /resource
-  # def update
-  #   super
-  # end
+  def after_inactive_sign_up_path_for(resource)
+    address_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :phone, :birthday])
+  end
 end
