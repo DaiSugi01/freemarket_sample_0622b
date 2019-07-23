df83ec9f633bc6c7b5ca867e73cd52729485fbaf
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-07-24 00:08:11 +0900

    address/newの単体テスト

--- spec/controllers/tbl_address_controller_spec.rb
+++ spec/controllers/tbl_address_controller_spec.rb
@@ -1,11 +1,38 @@
 require 'rails_helper'
 
-describe TblAddressController do
-  describe 'get#new' do
+describe TblAddressController, type: :controller do
+  describe 'get new' do
+
+    def request_new()
+      user = create(:tbl_user)
+      get :new, params: { tbl_user_id: user }
+    end
+
     it 'get @prefs' do
-      @prefs = create_list(:mst_prefecture, 47) 
-      get :new, params: { tbl_user_id: '13' }
+      request_new
+      prefs = create_list(:mst_prefecture, 47)
       expect(assigns(prefs)).to match(@prefs)
-    end  
+    end
+
+    it 'get @address' do
+      user = create(:tbl_user)
+      get :new, params: { tbl_user_id: user }
+      address = user.tbl_addresses.new()
+      expect(assigns(address)).to match(@address)
+    end
+
+    it 'render new' do
+      request_new
+      expect(response).to render_template :new
+    end
+
+  end
+
+  describe 'post create' do
+    let(:params) { { tbl_address: attributes_for(:tbl_address) } }
+
+    it '' do
+    end
+
   end
 end
