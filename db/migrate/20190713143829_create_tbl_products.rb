f7845818b8771f4c28fa944ad9050575337397c4
Daisugi <volble124@gmail.com>
2019-07-25 06:35:37 +0900

    delete-tbl_users

--- db/migrate/20190713143829_create_tbl_products.rb
+++ db/migrate/20190713143829_create_tbl_products.rb
@@ -2,7 +2,7 @@ class CreateTblProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :tbl_products do |t|
       t.string :name, null: false
-      t.string :description, null: false
+      t.text :description, null: false
       t.integer :price, null: false
       t.references :mst_major_category,  null: false, foreign_key: true
       t.references :mst_brand, foreign_key: true
@@ -13,7 +13,6 @@ class CreateTblProducts < ActiveRecord::Migration[5.2]
       t.references :mst_prefecture,  null: false, foreign_key: true
       t.references :mst_delivery_time,  null: false, foreign_key: true
       t.references :mst_status,  null: false, foreign_key: true
-      t.references :tbl_user,  null: false, foreign_key: true
       t.timestamps
     end
   end
