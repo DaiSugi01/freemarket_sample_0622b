01877843f9d15746e8983dcbe961a40d6ed153bf
yurifukuchi <uro_409@outlook.jp>
2019-07-25 23:09:12 +0900

    add product create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,10 +4,16 @@ class ProductsController < ApplicationController
   end
 
   def new
-    @products = TblProduct.new
+    @product = TblProduct.new
   end
   
   def create
+    TblProduct.create(product_params)
+      redirect_to root_path
   end
 
+  private
+  def product_params
+    params.require(:tbl_product).permit(:name, :description, :price)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'products#index'
-  resources :products, only:[:new,:create]
+  resources :products, only:[:new, :create]
   resources :cards, only: [:index]
 
 end
