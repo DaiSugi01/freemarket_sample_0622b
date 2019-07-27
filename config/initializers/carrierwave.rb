9be0fc7d2d4bf6ae268583a10343f397feae5117
yurifukuchi <uro_409@outlook.jp>
2019-07-28 03:17:05 +0900

    image.save to local

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,21 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-    aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-    region: 'ap-northeast-1' #例 'ap-northeast-1'
-  }
+  if Rails.env.production?
+    config.storage :file
+  else
+    config.storage :file
+  end
 
-  config.fog_directory  = 'freemarket-sample-0622b'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-sample-0622b'
+  # config.storage = :fog
+  # config.fog_provider = 'fog/aws'
+  # config.fog_credentials = {
+  #   provider: 'AWS',
+  #   aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+  #   aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+  #   region: 'ap-northeast-1' #例 'ap-northeast-1'
+  # }
+
+  # config.fog_directory  = 'freemarket-sample-0622b'
+  # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-sample-0622b'
 end
