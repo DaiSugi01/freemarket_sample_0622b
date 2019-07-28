f0ad9e2121c075df5e0a05d7e82e25dc5831c773
Daisugi <volble124@gmail.com>
2019-07-29 00:33:15 +0900

    add-users-controllers

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :products, only:[:new,:create]
   resources :cards, only: [:index]
 
+  # work ログアウト画面のパス。後ほど変更予定
+  get :signout, to: 'users#sign_out'
+
 end
