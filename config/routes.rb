56379173689c1b1e8ad0a3d61550f16133f92e5f
Daisugi <volble124@gmail.com>
2019-08-02 00:09:38 +0900

    add-route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
   root 'products#index'
-  resources :products, only:[:new,:create]
+  resources :products, only:[:new,:create, :destroy]
   resources :cards, only: [:index]
 
   # work ログアウト画面のパス。後ほど変更予定
   get :signout, to: 'users#sign_out'
+
+  # work 商品出品画面のパス。後ほど変更予定
+  get :sellproduct, to: 'users#sell_product'
 
 end
