97af6e5d44cac05aaf37c434c4d35a5c00f3360f
Daisugi <volble124@gmail.com>
2019-08-04 01:52:41 +0900

    add-controller-sell_product

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,9 @@ class UsersController < ApplicationController
   def mypage
   end
 
+  def sell_product
+    # work 対応状況実装後、条件を変更する。
+    @contents = TblProduct.where(tbl_user_id: current_tbl_user.id)
+  end
+  
 end
