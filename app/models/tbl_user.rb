c0f393193ead8688b4786f10c29511a1cb147dbe
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-08-09 22:53:07 +0900

    tbl_user model 設定

--- app/models/tbl_user.rb
+++ app/models/tbl_user.rb
@@ -2,7 +2,8 @@ class TblUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
 
   validates :last_name, presence: true
   validates :first_name, presence: true
