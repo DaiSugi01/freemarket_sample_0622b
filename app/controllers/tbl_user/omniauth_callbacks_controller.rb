ad1716ab352296c833e6869ed3015845195d945f
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-08-09 23:59:34 +0900

    omniauth controller(未実証）

--- app/controllers/tbl_user/omniauth_callbacks_controller.rb
+++ app/controllers/tbl_user/omniauth_callbacks_controller.rb
@@ -1,30 +1,17 @@
-# frozen_string_literal: true
-
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
 
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
+  def facebook
+    callback
+  end
 
-  # More info at:
-  # https://github.com/plataformatec/devise#omniauth
+  private
+  def callback
+    @user = TblUser.find_or_create_for_oauth(request.env['omniauth.auth'])
 
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
+    if @user.persisted?
+      redirect_to root_path
+    else
+      render new_tbl_user_session_path
+    end
+  end
 end
