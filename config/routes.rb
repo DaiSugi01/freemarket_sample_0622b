3d5f2c540b7cbfb248ba1423db3c0a9acf8d223b
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-08-11 21:03:25 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :tbl_users, :controllers => {
     registrations: 'tbl_user/registrations',
-    omniauth_callbacks: 'users/omniauth_callbacks',
+    omniauth_callbacks: 'tbl_user/omniauth_callbacks',
   }
   resources :tbl_users do
     resources :tbl_address, only: [:new, :create]
