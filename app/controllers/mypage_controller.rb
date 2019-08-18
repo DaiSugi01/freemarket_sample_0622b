25cf9be904f2972c97ff042cfa4c944ca38a0129
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-08-18 16:57:10 +0900

    user controllerをmypage controllerに変更

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,12 +1,32 @@
 class MypageController < ApplicationController
   before_action :redirect_to_top
 
-  def index
+  def signout
   end
+
+  def mypage
+  end
+
+  def sell_product
+    # work 対応状況実装後、条件を変更する。
+    @contents = TblProduct.where(tbl_user_id: current_tbl_user.id)
+  end
+
+  def sell_product_detail
+    @product_detail = TblProduct.find(params[:id])
+  end
+
+  def destroy
+    product = TblProduct.find(params[:id])
+    product.destroy
+    redirect_to action: :sell_product
+  end
+
+  private
 
   def redirect_to_top
     unless tbl_user_signed_in?
       redirect_to root_path
-    ena
+    end
   end
 end
