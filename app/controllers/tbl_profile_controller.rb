0d4e7857f0d2ac9aeb81c564e5d8a0f88998f29d
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-08-24 18:11:39 +0900

    プロフィール画面（実装途中）

--- app/controllers/tbl_profile_controller.rb
+++ app/controllers/tbl_profile_controller.rb
@@ -1,8 +1,14 @@
 class TblProfileController < ApplicationController
   def show
+    @profile = current_tbl_user.tbl_profile
   end
 
   def update
 
+  end
+
+  private
+  def tbl_profile_param
+    params.permit(:id, :nickaname, :icon, :description)
   end
 end
