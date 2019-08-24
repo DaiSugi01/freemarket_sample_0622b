fa155b9c747a2a3042f7bb2b04dc6be8197101c7
Daisugi <volble124@gmail.com>
2019-08-24 18:25:51 +0900

    modify-path

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -12,7 +12,7 @@ class CardsController < ApplicationController
       token = TblCard.create_card_to_payjp(params)
       card = TblCard.create({token: token.id, tbl_user_id: current_tbl_user.id})
       card.save
-      redirect_to card_path
+      redirect_to cards_path
     rescue
       redirect_to new_card_path, alert: 'クレジットカード登録に失敗しました'
     end
