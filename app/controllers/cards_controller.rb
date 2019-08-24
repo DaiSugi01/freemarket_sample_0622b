5211a541f0739cf70d309fbe201c2a45e563a5aa
Daisugi <volble124@gmail.com>
2019-08-24 14:33:11 +0900

    add-function-card-create

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -7,4 +7,14 @@ class CardsController < ApplicationController
     @card = TblCard.new
   end
 
+  def create
+    begin 
+      token = TblCard.create_card_to_payjp(params)
+      card = TblCard.create({token: token.id, tbl_user_id: current_tbl_user.id})
+      card.save
+      redirect_to card_path
+    rescue
+      redirect_to new_card_path, alert: 'クレジットカード登録に失敗しました'
+    end
+  end
 end
