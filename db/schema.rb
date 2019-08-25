44864348e97f1cc6ad51a34497dfcef22a8756b3
Daisugi <volble124@gmail.com>
2019-08-25 19:28:14 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_08_141024) do
+ActiveRecord::Schema.define(version: 2019_08_18_105355) do
 
   create_table "mst_brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
