87a4e1fe095f4367ee72fd9dc09c15c9467d0d8a
Daisugi <volble124@gmail.com>
2019-08-27 10:50:55 +0900

    add-delete-card-function-for-payjp

--- app/models/tbl_card.rb
+++ app/models/tbl_card.rb
@@ -20,6 +20,12 @@ class TblCard < ApplicationRecord
     Payjp::Customer.create(card: token.id)
   end
 
+  def self.destroy_card_of_payjp(card)
+    customer = Payjp::Customer.retrieve(card.token)
+    card = customer.cards.retrieve(customer.cards.data[0].customer)
+    card.delete
+  end
+
   def self.pay(amount,token_id)
     token = Payjp::Customer.retrieve(token_id)
     charge = Payjp::Charge.create(
@@ -27,5 +33,18 @@ class TblCard < ApplicationRecord
       customer: token.id,
       currency: 'jpy',
     )
+  end
+
+  def self.make_month(month)
+    #有効期限-月の値を加工する。
+    month = month.to_s
+    if month.length == 1
+      return "0" + month
+    end
+  end
+
+  def self.make_year(year)
+    #有効期限-年の値を加工する。
+    year = year.to_s.slice(2, 2)
   end
 end
