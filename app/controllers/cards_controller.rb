c5a2ee6ae89eb20da61b4552bc69692b97bec1cc
Daisugi <volble124@gmail.com>
2019-08-27 10:49:38 +0900

    modify-create-function

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -8,9 +8,16 @@ class CardsController < ApplicationController
   end
 
   def create
-    begin 
+    begin
       token = TblCard.create_card_to_payjp(params)
-      card = TblCard.create({token: token.id, tbl_user_id: current_tbl_user.id})
+      expire_month = TblCard.make_month(token.cards.data[0].exp_month)
+      expire_year = TblCard.make_year(token.cards.data[0].exp_year)
+      card = TblCard.create({brand:token.cards.data[0].brand,
+                             expire_month: expire_month,
+                             expire_year: expire_year,
+                             last_four_number: token.cards.data[0].last4.to_s,
+                             token: token.id, 
+                             tbl_user_id: current_tbl_user.id})
       card.save
       redirect_to cards_path
     rescue => e
